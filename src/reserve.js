40db55a43acec1898593799f25f69b705a9b8e34
jisuyoun <nigayoun@gmail.com>
2017-12-21 12:15:45 +0900

    add. 예약 카운트에 따른 숫자증감

--- src/reserve.js
+++ src/reserve.js
@@ -1,4 +1,81 @@
 
+
+class TicketReservation {
+	constructor() {
+		this.matchedClassNameList = ["ico_minus3","ico_plus3"];
+		this.DATA = {
+			disabled : 'disabled'
+		}
+	}
+
+	run() {
+		this.regCountBtn();
+	}
+
+	regCountBtn() {
+		const elTicketBody = document.querySelector(".ticket_body");
+		elTicketBody.addEventListener("click", (e) => {
+			e.preventDefault();
+			const target = e.target;
+			if(!this._isCountBtn(target)) return;
+			const btnType = (target.classList.contains(this.matchedClassNameList[0])) ? "-" : "+";
+
+			this._resetCountValue(btnType, target);
+		});
+	}
+
+	_isCountBtn(target) {
+		return this.matchedClassNameList.some( (className) => {
+			return target.classList.contains(className);
+		});
+	}
+
+	_resetCountValue(btnType, target) {
+		const elCountValue = target.parentElement.querySelector(".count_control_input");
+		let countValue = +elCountValue.value;
+		if(btnType === "+") { 
+			countValue++;
+		} else { 
+			if(countValue === 0)  {
+				this.addClass(elCountValue, this.DATA.disabled);
+				return countValue;;
+			}
+			countValue--;
+		}
+
+		this.setCountValue(elCountValue, countValue)
+			  .removeClass(elCountValue, this.DATA.disabled)
+			  .updatePartialPrice();
+	}
+
+	setCountValue(elCountValue, countValue) {
+		elCountValue.value = countValue;
+		return this;
+	}
+
+	addClass(elCountValue, nextClass) {
+		const elMinus =elCountValue.previousElementSibling; 
+		[elCountValue, elMinus].forEach( el => {
+			el.classList.add(nextClass);
+		});
+		return this;
+	}
+
+	removeClass(elCountValue, nextClass) {
+		const elMinus =elCountValue.previousElementSibling; 
+		[elCountValue, elMinus].forEach( el => {
+			el.classList.remove(nextClass);
+		});
+		return this;
+	}
+
+	updatePartialPrice(target) {
+
+	}
+
+} //end of class
+
 document.addEventListener("DOMContentLoaded", evt => {
-console.log('loaded');
+	const tr = new TicketReservation();
+	tr.run();
 });
