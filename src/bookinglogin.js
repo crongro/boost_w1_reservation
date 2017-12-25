1e8b3bd565c301995b249fea092d57e6fac649c9
jisuyoun <nigayoun@gmail.com>
2017-12-25 16:43:39 +0900

    add. 예약취소 확인UI및 갯수동기화

--- src/bookinglogin.js
+++ src/bookinglogin.js
@@ -1,16 +1,28 @@
 import * as common from './common.js';
 
+var sEmail = localStorage.getItem("email");
+if(sEmail !== null) location.href="./myreservation.html"
 
-document.querySelector(".login_btn").addEventListener("click", function(e) {
-	e.preventDefault();
-	const reg = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
-	const email = document.querySelector("#resrv_id").value;
-	if(reg.test(email)) {
-		localStorage.setItem('email', email);
-		document.getElementById("form1").submit();
-	}else {
-		alert("올바른 이메일 형식이 아닙니다");
-	}
+
+function checkEmailValue() {
+	document.querySelector(".login_btn").addEventListener("click", function(e) {
+		e.preventDefault();
+		const reg = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+		const email = document.querySelector("#resrv_id").value;
+		if(reg.test(email)) {
+			localStorage.setItem('email', email);
+			document.getElementById("form1").submit();
+		}else {
+			alert("올바른 이메일 형식이 아닙니다");
+		}
+	});
+}
+
+
+document.addEventListener("DOMContentLoaded", (evt) => {
+	checkEmailValue();
 });
+
+
 
 
--- src/myreservation.js
+++ src/myreservation.js
@@ -1,10 +1,10 @@
 import * as common from './common.js';
 
-function checkLogin() {
+(function(){
 	const email = common.getEmail();
 	const elViewReservation  = document.querySelector(".viewReservation");
 	if(email === null) location.href = "./bookinglogin.html";
-}
+})();
 
 function checkLoginFromStorage() {
 	const email = common.getEmail();
@@ -12,10 +12,29 @@ function checkLoginFromStorage() {
 	if(email !== null) elViewReservation.innerText = email;
 }
 
-checkLogin();
+function cancelReservation() {
+	 const elConfirmCard = document.querySelector("li.confirmed");
+	 const elCancelWrap = document.querySelector("li.cancel");
+
+	 elConfirmCard.addEventListener("click", ({target : t}) => {
+	 	if(t.className === "btn" || (t.tagName ==="SPAN" && t.parentElement.className === "btn")) {
+	 		const result = window.confirm('취소하시겠습니까?');
+	 		const elCurrentArticle = t.closest("article");
+	 		elCancelWrap.appendChild(elCurrentArticle);
+	 		elCurrentArticle.querySelector(".booking_cancel").style.display = 'none';
+
+	 		const elSS = document.querySelector(".summary_board li.item:nth-child(2) .figure");
+	 		elSS.innerText = +elSS.innerText - 1;
+
+	 		const elBack = document.querySelector(".summary_board li.item:nth-child(4) .figure");
+	 		elBack.innerText = +elBack.innerText + 1;
+	 	}
+	 });
+}
 
 document.addEventListener("DOMContentLoaded", (evt) => {
 	checkLoginFromStorage();
+	cancelReservation();
 });
 
 
